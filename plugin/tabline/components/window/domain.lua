116c016947b02ca4e24f782c91d8bfe77b8b2c2e
Quentin Revel <qrevel@nottle.fr>
2026-04-04 03:27:59 +0200

    feat(components): add configurable domain icons

--- plugin/tabline/components/window/domain.lua
+++ plugin/tabline/components/window/domain.lua
@@ -11,15 +11,21 @@ return {
       docker = wezterm.nerdfonts.md_docker,
       unix = wezterm.nerdfonts.cod_terminal_linux,
     },
+    domain_name_to_icon = {},
   },
   update = function(window, opts)
     local domain_name = window:active_pane():get_domain_name()
     local domain_type, new_domain_name = domain_name:match('^([^:]+):?(.*)')
+    local domain_name_to_icon = opts.domain_name_to_icon or {}
+    local domain_to_icon = opts.domain_to_icon or {}
     domain_type = (domain_type or 'default'):lower()
     new_domain_name = new_domain_name ~= '' and new_domain_name or domain_name
 
     if opts.icons_enabled and opts.domain_to_icon then
-      local icon = opts.domain_to_icon[domain_type] or opts.domain_to_icon.default
+      local icon = domain_name_to_icon[new_domain_name]
+        or domain_to_icon[domain_type]
+        or domain_name_to_icon.default
+        or domain_to_icon.default
       util.overwrite_icon(opts, icon)
     end
     return new_domain_name
